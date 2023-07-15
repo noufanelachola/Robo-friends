03f650b5f1fa0d38f1eb0edcbb13088992cd14e7
noufanelachola <noufansadiqelachola@gmail.com>
2023-07-15 05:56:32 +0530

    adding upto the errorboundary

--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -4,6 +4,7 @@ import CardList from '../components/CardList';
 // import {robots} from '../components/robots';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 
 
 class App extends Component {
@@ -40,7 +41,9 @@ class App extends Component {
         <SearchBox searchChange={this.onSearchChange} />
 
         <Scroll>
-          <CardList robots={filteredRobots}/>
+          <ErrorBoundary>
+            <CardList robots={filteredRobots}/>
+          </ErrorBoundary>
         </Scroll>
 
       </div>
